ca2bbdeb272b106b18c4a0384a9e746bb28435e7
Taylor MacDonald <tcmacdonald@gmail.com>
2017-06-11 21:23:43 -0400

    Implement rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,13 @@ Bundler.require(*Rails.groups)
 
 module CincinnatiSouthern
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
