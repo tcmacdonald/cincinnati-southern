72090ed8a84fc00cc46eb6ec88c2c38d63551f4b
Taylor MacDonald <tcmacdonald@gmail.com>
2017-06-18 14:01:37 -0400

    Implement rspec & Guard

--- Gemfile
+++ Gemfile
@@ -5,10 +5,6 @@ gem 'sqlite3'
 gem 'puma', '~> 3.0'
 gem 'rack-cors'
 
-group :development, :test do
-  gem 'byebug', platform: :mri
-end
-
 group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
@@ -17,5 +13,17 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '>= 3.5.0'
+  gem 'pry-rails'
+end
+
+group :test do
   gem 'factory_girl_rails'
+  gem 'guard-rspec'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'capybara'
+  gem 'poltergeist', git: 'https://github.com/ample/poltergeist.git'
+  gem 'shoulda-matchers', require: false
+  gem 'vcr'
+  gem 'webmock'
 end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+require 'database_cleaner'
+require 'factory_girl'
+require 'shoulda/matchers'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  Capybara.javascript_driver = :poltergeist
+  Capybara.default_host = 'http://app.example.com'
+
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+  config.include Shoulda::Matchers::ActiveModel, type: :model
+  config.include FactoryGirl::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
